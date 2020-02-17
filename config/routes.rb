686eeccc409d4a41e048a9258ab0c46356ce0265
kuzmich-1992 <kuzmichman92@gmail.com>
2020-02-17 21:32:45 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  resources :users
   end
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
